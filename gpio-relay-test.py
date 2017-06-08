1336374529dc19f7f40c6bdd943a8ce6cebc4472
Matthew <mattrachwal@gmail.com>
2017-06-08 02:34:20 -0500

    Uploaded 8 channel relay test script

---
+++ gpio-relay-test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# initiate list with pin gpio pin numbers
+# GPIO | Relay
+#--------------
+# 26     01
+# 19     02
+# 13     03
+# 06     04
+# 12     05
+# 16     06
+# 20     07
+# 21     08
+
+gpioList = [26, 19, 13, 06, 12, 16, 20, 21]
+
+for i in gpioList:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, GPIO.HIGH)
+
+# Sleep time variables
+
+sleepTimeShort = 0.2
+sleepTimeLong = 0.1
+
+# MAIN LOOP =====
+# ===============
+
+try:
+    while True:
+        for i in gpioList:
+            GPIO.output(i, GPIO.LOW)
+            time.sleep(sleepTimeShort);
+            GPIO.output(i, GPIO.HIGH)
+            time.sleep(sleepTimeLong);
+
+
+# End program cleanly with keyboard
+except KeyboardInterrupt:
+    print " Quit"
+
+    # Reset GPIO settings
+
+    GPIO.cleanup()
+
